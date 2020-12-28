04d4e5f6006803044c7ec64cf65d1464c9edb853
Minato86 <doronomori660@outlook.jp>
2020-12-28 20:50:19 +0900

    update: 旧サイトをsvelteに移行

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,118 @@
 <script>
-	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    @import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@1&family=M+PLUS+Rounded+1c&display=swap");
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    .header {
+        padding: 12% 10%;
+        background: linear-gradient(
+            73.69690465093572deg,
+            rgba(12, 48, 89, 1) 19.997395833333332%,
+            rgba(11, 48, 89, 1) 19.997395833333332%,
+            rgba(0, 174, 237, 1) 57.393229166666664%
+        );
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h1 {
+        font-family: "Josefin Sans", sans-serif;
+    }
+
+    .header p,
+    .header h1 {
+        color: white;
+        font-family: "Josefin Sans", sans-serif;
+    }
+
+    .about {
+        text-align: center;
+        padding: 7% 0 0 0;
+    }
+
+    .about p {
+        font-family: "M PLUS Rounded 1c", sans-serif;
+    }
+
+    .about a {
+        font-family: "Josefin Sans", sans-serif;
+        padding: 0.6em 2em;
+        text-decoration: none;
+        color: #67c5ff;
+        border: solid 2px #67c5ff;
+        border-radius: 5px;
+        transition: 0.4s;
+    }
+
+    .about a:hover {
+        background: #67c5ff;
+        color: white;
+    }
+
+    .works {
+        text-align: center;
+        padding: 7% 0;
+    }
+
+    .works a {
+        text-decoration: none;
+        font-family: "Josefin Sans", sans-serif;
+        font-size: 1.4em;
+        color: #67c5ff;
+        transition: 0.4s;
+    }
+
+    .works a:hover {
+        color: #a1dbff;
+    }
+
+    .footer {
+        text-align: center;
+        color: black;
+        padding: 1em 0;
+        font-family: "Josefin Sans", sans-serif;
+    }
 </style>
+
+<main>
+    <div class="header">
+        <h1>Surume's Home</h1>
+        <p>MotionGraphics / 3DCG</p>
+    </div>
+    <div class="about">
+        <h1>About</h1>
+        <hr width="30px" />
+        <p style="margin-top: 3em;">
+            MotionGraphicsや3DCGを用いた映像制作を行っています。<br />
+            するめはおいしいですね。
+        </p>
+        <p style="margin-bottom: 2em;">
+            使用ソフトウェアはAfterEffectsとBlenderです。<br />
+            現在依頼募集中です。
+        </p>
+        <a href="https://twitter.com/ssuurruummee_" target="_blank">Twitter</a>
+    </div>
+    <div class="works">
+        <h1>Works</h1>
+        <hr width="30px" />
+        <div style="margin-top: 3em; margin-bottom: 2em;">
+            <a
+                href="https://www.youtube.com/watch?v=V1R3gxuAQbc"
+                target="_blank"><img
+                    src="https://i.gyazo.com/02aec64c723ac4018cc49059710b2fc4.jpg" /></a>
+            <a
+                href="https://www.youtube.com/watch?v=MnBeNOC-l6o"
+                target="_blank"><img
+                    src="https://i.gyazo.com/603c1e97da77418edd5b05de860de02a.jpg" /></a>
+            <a
+                href="https://www.youtube.com/watch?v=lvE_vcUfc1g"
+                target="_blank"><img
+                    src="https://i.gyazo.com/85aac511c6bf0ed89599a813d0495d9a.jpg" /></a>
+        </div>
+        <a
+            href="https://www.youtube.com/channel/UCu2woK-ifovek0Yps5aGQHA?view_as=subscriber"
+            target="_blank">More works</a>
+    </div>
+    <div class="footer">
+        <p>&copy; surume 2020 / Design by Minato minami</p>
+    </div>
+</main>
